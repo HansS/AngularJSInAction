7a681485c7b91c00fd389da15cd58b107c6ff8a7
Joseph Palmour <jpalmour@gmail.com>
2013-10-21 11:34:01 -0400

    added more tests

--- angello/tests/angelloModelSpec.js
+++ angello/tests/angelloModelSpec.js
@@ -1,16 +1,51 @@
 describe('Service: angelloModel', function() {
 	beforeEach(module('Angello'));
+
 	var modelService;
+
 	beforeEach(inject(function (angelloModel) {
 		modelService = angelloModel;
 	}));
 
 	describe('#getStatuses', function() {
-		// it('1+1 should equal 2', function() {
-		// 	expect(1+1).toBe(2);
-		// });
 		it('should return 7 statuses', function () {
 			expect(modelService.getStatuses().length).toBe(7);
 		});
+
+		it('should have a status named "To Do"', function () {
+			expect(modelService.
+				getStatuses().
+				map(function (status) {
+					return status.name;
+				})).
+				toContain('To Do');
+		});
 	});
+
+	describe('#getTypes', function() {
+		it('should return 4 types', function () {
+			expect(modelService.getTypes().length).toBe(4);
+		});
+
+		it('should have a type named "Bug"', function() {
+			expect(modelService.getTypes().
+				map(function(type) {
+					return type.name;
+				})).
+				toContain('Bug');
+		});
+	});
+
+	describe('#getStories', function() {
+		it('should return 3 stories', function () {
+			expect(modelService.getStories().length).toBe(3);
+		});
+
+		it('should return stories that have a description property', function() {
+			modelService.getStories().forEach(function (story) {
+				expect(story.description).toBeDefined();
+			})
+		})
+	});
+
 });
