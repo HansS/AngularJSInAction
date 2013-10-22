4773bba96b4dd4bde85ac47ddac3aec77b5fcd72
Joseph Palmour <jpalmour@gmail.com>
2013-10-22 11:36:47 -0400

    decomposed controllers to multiple files

--- angello/js/app.js
+++ angello/js/app.js
@@ -1,87 +1,5 @@
 var myModule = angular.module('Angello', []);
 
-myModule.factory('angelloModel',function() {
-
-	var getStatuses = function() {
-		var tempArray = [
-			{name:'Back Log'},
-			{name:'To Do'},
-			{name:'In Progress'},
-			{name:'Code Review'},
-			{name:'QA Review'},
-			{name:'Verified'},
-			{name:'Done'}
-		];
-		return tempArray;
-	};
-
-	var getTypes = function() {
-		var tempArray= [
-			{name:'Feature'},
-			{name:'Enhancement'},
-			{name:'Bug'},
-			{name:'Spike'}
-		];
-		return tempArray;
-	};
-	
-	var getStories = function() {
-		var tempArray = [
-			{
-				title:'Story 00', 
-				description:'Description pending.',
-				criteria:'Criteria pending.',
-				status:'To Do',
-				type:'Feature',
-				reporter:'Lukas Ruebbelke',
-				assignee:'Brian Ford'
-			},
-			{
-				title:'Story 01', 
-				description:'Description pending.',
-				criteria:'Criteria pending.',
-				status:'In Progress',
-				type:'Feature',
-				reporter:'Lukas Ruebbelke',
-				assignee:'Brian Ford'
-			},
-			{
-				title:'Story 02', 
-				description:'Description pending.',
-				criteria:'Criteria pending.',
-				status:'Code Review',
-				type:'Bug',
-				reporter:'Lukas Ruebbelke',
-				assignee:'Brian Ford'
-			}
-		];
-		return tempArray;
-	};
-
-	return {
-		getStatuses: getStatuses,
-		getTypes: getTypes,
-		getStories: getStories
-	}
-
-});
-
-myModule.factory('angelloHelper', function() {
-	
-	var buildIndex = function(source, property) {
-		var tempArray = [];
-		for(var i = 0, len = source.length; i < len; ++i) {
-			tempArray[source[i][property]] = source[i];
-		}
-		return tempArray;
-	};
-
-	return {
-		buildIndex: buildIndex
-	};
-
-});
-
 myModule.controller('MainCtrl', 
 	function($scope, angelloModel, angelloHelper) { 
 		$scope.currentStory;
@@ -90,7 +8,6 @@ myModule.controller('MainCtrl',
 		$scope.stories = angelloModel.getStories();
 		$scope.typesIndex = angelloHelper.buildIndex($scope.types, 'name');
 		$scope.statusesIndex = angelloHelper.buildIndex($scope.statuses, 'name');
-
 		$scope.currentStatus;
 		$scope.currentType;
 		
